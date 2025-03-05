5bc8b293ed533a5621a92474969448653e4d6653
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-06 05:38:13 +0900

    add : HTTP POST, HTTP GETの汎用コードを追加

---
+++ internal/api/post.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+func Post(reqeustData any, url string, useSessionId bool) (statusCode string, responseBody []byte, err error) {
+	// JSONにエンコード
+	jsonreqeustData, err := json.Marshal(reqeustData)
+	if err != nil {
+		fmt.Println("JSONエンコードエラー:", err)
+		return
+	}
+	// HTTPクライアントの作成
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	// POSTリクエストの作成
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonreqeustData))
+	if err != nil {
+		fmt.Println("リクエスト作成エラー:", err)
+		return
+	}
+	// ヘッダーを設定
+	req.Header.Set("Content-Type", "application/json")
+
+	// リクエスト送信
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("リクエスト送信エラー:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// レスポンスの読み取り
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("レスポンス読み取りエラー:", err)
+		return
+	}
+
+	// 値返却
+	statusCode = resp.Status
+	responseBody = body
+	return
+}
